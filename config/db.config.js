02a8643ffe9d5b4b1406d1d430e7d679a65e5919
FredSilva21 <fredsilva43@gmail.com>
2024-12-15 20:30:23 +0000

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require("sequelize")
 
-const db_name =  process.env.MySQL_DB_NAME
-const db_user =  process.env.MySQL_DB_USER
-const db_password =  process.env.MySQL_DB_PASSWORD
-const db_host = process.env.MySQL_DB_HOST
+const db_name =  process.env.MySQL_DB_NAME || "celebr8"
+const db_user =  process.env.MySQL_DB_USER || "root"
+const db_password =  process.env.MySQL_DB_PASSWORD || "root"
+const db_host = process.env.MySQL_DB_HOST   || "127.0.0.1"
+const db_port = process.env.MySQL_DB_PORT || 3306
 const db_dialect = 'mysql'
 
-const sequelize = new Sequelize( db_name, db_user, db_password, {
+const sequelize = new Sequelize( db_name, db_user, db_password, db_port, {
     host: db_host,
+    port: db_port,
     dialect: db_dialect
 });
 
